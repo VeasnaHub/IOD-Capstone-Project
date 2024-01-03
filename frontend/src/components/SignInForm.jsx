d40d76dcbead84188a72c174121f8dfac94f17f5
VeasnaHub <veasna.evan@gmail.com>
2024-01-03 21:46:42 +1300

    updated the component to send request to back end

--- frontend/src/components/SignInForm.jsx
+++ frontend/src/components/SignInForm.jsx
@@ -1,16 +1,67 @@
 import { useNavigate } from "react-router-dom";
+import { useUserContext } from "../context/UserContext";
+import { useState } from "react";
+import axios from "axios";
 
 function SignInForm() {
-
+    const { currentUser, handleUpdateUser } = useUserContext();
+    const [loggedIn, setLoggedIn] = useState(currentUser.firstName);
+    const [errMsg, setErrMsg] = useState('');
+    const [loginAttempts, setLoginAttempts] = useState(0);
     const navigate = useNavigate();
 
+    console.log(currentUser)
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const data = new FormData(event.currentTarget);
+       
+        let userEmail = data.get('email')
+        let userPassword = data.get('password')
+
+        let loggedInUser = null;
+
+        try {
+            let response = await axios.post('/api/users/login', {email: userEmail, password: userPassword});
+            loggedInUser = response.data.data;
+            console.log(loggedInUser)
+
+        } catch (err) {
+            console.log(err.message)
+            setErrMsg(err.message + ': ' + err.response.data.result);
+        }
+
+        if (!loggedInUser) {
+            let newAttempts = loginAttempts + 1
+
+            if (newAttempts === 5) {
+                setErrMsg('Maximum login attempts exceeded. You are blocked.');
+            }
+            else {
+                setErrMsg('Unsuccessful login attempt #' + newAttempts + ' of 5');
+            }
+            setLoginAttempts(newAttempts)
+            setLoggedIn(false)
+        } else {
+            setErrMsg('')
+            handleUpdateUser(loggedInUser)
+            setLoggedIn(true)
+            navigate('/trips')
+        }
+    };
+    
     return (
-        <form className="SignInForm">
-            <h1 className="l-font green-font bold-font">SIGN <span className="l-font yellow-font bold-font">IN</span></h1>
-            <input placeholder="Email"/>
-            <input placeholder="Password"/>
-            <button className="yellow-button" onClick={() => navigate("/trips")}>SUBMIT</button>
-        </form>
+        <>
+        { (!loggedIn) ?
+            <form className="SignInForm" onSubmit={handleSubmit}>
+                <h1 className="l-font green-font bold-font">SIGN <span className="l-font yellow-font bold-font">IN</span></h1>
+                <input name="email" id="email" placeholder="Email"/>
+                <input name="password" id="password" placeholder="Password"/>
+                <button className="yellow-button" type="submit">SUBMIT</button>
+            </form>
+        : navigate('/trip') }
+        </>
     )
 }
 
