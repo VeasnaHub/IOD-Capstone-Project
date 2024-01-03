5b38e3a07de8faf68be3e229c90f8e48fe217589
VeasnaHub <veasna.evan@gmail.com>
2024-01-03 21:45:13 +1300

    updated the component to be able send request to backend

--- frontend/src/components/CompleteProfileBody.jsx
+++ frontend/src/components/CompleteProfileBody.jsx
@@ -1,28 +1,49 @@
 import { useNavigate } from "react-router-dom";
-import Button from "./Button";
+import { useUserContext } from "../context/UserContext";
+import axios from "axios";
 
 function CompleteProfileBody() {
 
+    const {currentUser, handleUpdateUser} = useUserContext();
     const navigate = useNavigate();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const data = new FormData(event.currentTarget);
+
+            const response = await axios.put(`/api/users/${currentUser.id}`, Object.fromEntries(data.entries()))
+            const userUpdate = response.data.data;
+    
+            if (userUpdate) {
+                handleUpdateUser({ ...currentUser, ...response.data.data });
+                console.log(userUpdate);
+                navigate('/registration');
+                }
+        } catch (error) {
+                console.log(error)
+        };
+    };
 
     return (
         <div className="CompleteProfileBody in-body">
             <div>
                 <h1>To insert placeholder for profile photo</h1>
-                <Button background="yellow-button" name="UPDATE PHOTO" />
+                <button className="yellow-button">UPLOAD PHOTO</button>
             </div>
-            <div>
+            <form className="complete-profile-form" onSubmit={handleSubmit}>
                 <h1 className="green-font bold-font">COMPLETE YOUR PROFILE</h1>
-                <label>First Name:</label><br/>
-                <input type="text"></input><br/>
-                <label>Last Name:</label><br/>
-                <input type="text"></input><br/>
-                <label>Base Location:</label><br/>
-                <input type="text"></input><br/>
-                <label>Profile Summary:</label><br/>
-                <input type="text"></input><br/>
-                <button className="green-button" onClick={() => navigate("/registration")}>SAVE</button>
-            </div>
+                <label>First Name:</label>
+                <input type="text" name="firstName" id="firstName"></input>
+                <label>Last Name:</label>
+                <input type="text" name="lastName" id="lastName"></input>
+                <label>Base Location:</label>
+                <input type="text" name="baseLocation" id="baseLocation"></input>
+                <label>Profile Summary:</label>
+                <input type="text" name="profileSummary" id="profileSummary"></input>
+                <button className="green-button" type="submit">SAVE</button>
+            </form>
         </div>
     )
 }
