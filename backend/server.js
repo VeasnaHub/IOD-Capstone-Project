4d4383f67975b1478c8e238828fc751b65a17fff
VeasnaHub <veasna.evan@gmail.com>
2024-01-03 21:39:46 +1300

    set up env config

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,24 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
+const cors = require('cors');
+const path = require('path');
 
-require("dotenv").config();
+const dotenv = require('dotenv');
+const environment = process.env.NODE_ENV || "local";
+
+dotenv.config({ path: `./.env.${environment}` });
+
 let dbConnect = require("./dbConnect");
 
 app.use(express.json());
+app.use(cors());
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Welcome to Waka application."});
-});
-
-const PORT = process.env.PORT || 8080;
+// app.get("/", (req, res) => {
+//     res.json({
+//         message: "Welcome to Waka application."});
+// });
 
 let userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
@@ -21,6 +28,8 @@ app.use('/api/trips', tripRoutes);
 
 let bookingRoutes = require('./routes/bookingRoutes');
 app.use('/api/bookings', bookingRoutes);
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
