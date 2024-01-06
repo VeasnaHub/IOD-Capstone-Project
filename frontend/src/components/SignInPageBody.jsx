139bef7142e29233cd4ef7f16b1fa59e40935016
VeasnaHub <veasna.evan@gmail.com>
2024-01-06 15:11:51 +1300

    Initial commit

---
+++ frontend/src/components/SignInPageBody.jsx
@@ -0,0 +1,18 @@
+import Logo from "./logo";
+import SignInForm from "./SignInForm";
+
+function SignInPageBody() {
+    return (
+        <div className="SignInPageBody">
+            <div className="body-item">
+                <p className="l-font green-font light-font">Driving green, sharing smiles with <Logo />!</p>
+                <img className="body-item" src="../src/assets/Signinpage.jpg" style={{ width: "100%", height: "auto" }}/>
+            </div>
+            <div className="body-item">
+                <SignInForm />
+            </div>
+        </div>
+    )
+}
+
+export default SignInPageBody;
