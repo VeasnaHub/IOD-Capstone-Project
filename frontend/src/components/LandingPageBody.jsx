3e94b8270e2f4c0d93ebd8960c019e45a5b7220a
VeasnaHub <veasna.evan@gmail.com>
2024-01-06 15:09:19 +1300

    modified a better UI

--- frontend/src/components/LandingPageBody.jsx
+++ frontend/src/components/LandingPageBody.jsx
@@ -6,10 +6,10 @@ function LandingPageBody() {
     const navigate = useNavigate();
 
     return (
-        <div className="LandingPageBody body-container">
+        <div className="LandingPageBody">
             <div className="body-item">
                 <p className="l-font green-font light-font">Introducing <Logo />, your eco-friendly companion for shared commuting!</p>
-                <p style={{lineHeight: "2"}}>Embrace sustainable travel with our 'Go Green' initiative, connecting users seamlessly for eco-conscious carpooling. With a sleek interface, and acommitment to environmental responsibility, we make every ride a step towards a cleaner, healthier planet.</p>
+                <p style={{lineHeight: "1.3"}}>Embrace sustainable travel with our 'Go Green' initiative, connecting users seamlessly for eco-conscious carpooling. With a sleek interface, and acommitment to environmental responsibility, we make every ride a step towards a cleaner, healthier planet.</p>
                 <p className="green-font bold-font">Join us in reducing carbon footprints, one shared journey at a time!</p>
                 <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
                     <p className="l-font green-font light-font">Go carpooling, Go <Logo />, Go</p>
@@ -17,7 +17,7 @@ function LandingPageBody() {
                 </div>
             </div>
             <div className="body-item">
-                <img src="../src/assets/Homepage.jpg" alt="photo of yellow car and people" style={{ width: "100%", height: "auto" }}/>
+                <img src="../src/assets/Homepage.jpg" alt="photo of yellow car and people" style={{ width: "110%", height: "auto" }}/>
             </div>
         </div>
     )
