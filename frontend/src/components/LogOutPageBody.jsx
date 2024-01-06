06d4de4f173a05f859812fcf5e2690e396f559e1
VeasnaHub <veasna.evan@gmail.com>
2024-01-06 15:10:18 +1300

    modified style for better UI

--- frontend/src/components/LogOutPageBody.jsx
+++ frontend/src/components/LogOutPageBody.jsx
@@ -2,13 +2,13 @@ import Logo from "./logo";
 
 function LogOutPageBody() {
     return (
-        <div className="LogOutPageBody body-container">
+        <div className="LogOutPageBody">
             <div className="body-item">
                 <p className="l-font green-font">You're contributing to a <span className="green-font l-font bold-font">GREENER </span><span className="yellow-font l-font bold-font">FUTURE</span>, with <Logo />.</p>
                 <p className="l-font green-font">Safe travels, and thank you for being a part of our <span className="green-font l-font bold-font">COMMUNIT</span><span className="yellow-font l-font bold-font">Y</span>!"</p>
             </div>
             <div className="body-item">
-                <img src="./src/assets/Logoutpage.jpg" style={{ width: "100%", height: "auto" }}/>
+                <img src="./src/assets/Logoutpage.jpg" style={{ width: "110%", height: "auto" }}/>
             </div>
         </div>
     )
