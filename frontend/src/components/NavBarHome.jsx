a99b7bd40ff2a361f660fc1aac44a1d04a85b01f
VeasnaHub <veasna.evan@gmail.com>
2024-01-06 15:10:40 +1300

    Added code for a responsive nav bar

--- frontend/src/components/NavBarHome.jsx
+++ frontend/src/components/NavBarHome.jsx
@@ -1,21 +1,28 @@
 import Logo from "./logo";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function NavBarHome() {
-
   const navigate = useNavigate();
-  
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className="NavBarHome">
+    <div className={`NavBarHome ${isMenuOpen ? "menu-open" : ""}`}>
       <Logo />
-      <nav className="nav-container">
+      <nav className={`nav-container ${isMenuOpen ? "menu-open" : ""}`}>
         <button className="default-button" onClick={() => navigate("/")}>HOME</button>
         <button className="default-button">HOW IT WORKS</button>
-        <button className="default-button">CONTACT US</button>
+        <button className="default-button">COMMUNITY</button>
         <button className="default-button" onClick={() => navigate("/signin")}>SIGN IN</button>
         <button className="green-button" onClick={() => navigate("/signup")}>SIGN UP</button>
       </nav>
+      <button className="hamburger-icon" onClick={handleMenuToggle}>☰</button>
     </div>
-  )
+  );
 }
+
 export default NavBarHome;
