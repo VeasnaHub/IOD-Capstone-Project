284e0a643ecf730c8a7457d12027d5f0496b321f
VeasnaHub <veasna.evan@gmail.com>
2024-01-08 21:55:28 +1300

    Added the function to fetch the data and displays it

--- frontend/src/components/BookingList.jsx
+++ frontend/src/components/BookingList.jsx
@@ -1,53 +1,77 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import { useUserContext } from "../context/UserContext";
 import useBookings from "../hooks/useBookings";
-
+import { useNavigate } from "react-router-dom";
 
 function BookingList() {
-    const { currentUser } = useUserContext();
-    const { bookings, message, fetchData } = useBookings(currentUser);
+  const { currentUser } = useUserContext();
+  const { bookings, message, fetchData } = useBookings(currentUser);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    return (
-        <div className="BookingList in-body">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Booking #</th>
-                        <th>Trip #</th>
-                        <th>Departure</th>
-                        <th>Destination</th>
-                        <th>Service Day</th>
-                        <th>Time</th>
-                        <th>Seats</th>
-                        <th>Unit Price</th>
-                        <th>Status</th>
-                        <th className="action-column">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td className="action-column">
-                            <button className="yellow-button">Cancel</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+  return (
+    <div className="BookingList in-body">
+      {bookings.length === 0 ? (
+        <div className="no-booking">
+          <div className="body-item">
+            <p>You have not request any booking yet!</p>
+            <p>
+              Make a booking now?{" "}
+              <button
+                className="green-button"
+                onClick={() => navigate("/trips")}
+              >
+                CLICK HERE
+              </button>
+            </p>
+          </div>
+          <img
+            className="body-item"
+            src="../src/assets/Nobookingpage.jpg"
+            style={{ width: "400px", height: "auto" }}
+          />
         </div>
-    )
+      ) : (
+        <table className="fixed-size-table booking-status-table">
+          <thead>
+            <tr>
+              <th>Booking #</th>
+              <th className="trip">Trip #</th>
+              <th>Departure</th>
+              <th>Destination</th>
+              <th className="service-day">Service Day</th>
+              <th className="departure-time">Time</th>
+              <th className="seats">Requested Seats</th>
+              <th className="unit-price">Unit Price</th>
+              <th>Status</th>
+              <th className="action-column-small">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.map((booking) => (
+              <tr key={booking.id}>
+                <td>{booking.id}</td>
+                <td className="trip">{booking.tripId}</td>
+                <td>{booking.trip.departure}</td>
+                <td>{booking.trip.destination}</td>
+                <td className="service-day">{booking.trip.serviceDay}</td>
+                <td className="departure-time">{booking.trip.departureTime}</td>
+                <td className="seats">{booking.requestedSeat}</td>
+                <td className="unit-price">{booking.trip.unitPrice}</td>
+                <td>{booking.status}</td>
+                <td className="action-column-small">
+                  <button className="yellow-button">Cancel Request</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
 
 export default BookingList;
