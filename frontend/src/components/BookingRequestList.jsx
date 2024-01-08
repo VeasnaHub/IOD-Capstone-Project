032def10c3f01622d8324791849604d4085d292f
VeasnaHub <veasna.evan@gmail.com>
2024-01-08 21:56:32 +1300

    added code to perform a decrement operation on the available seats upon the confirmation of booking

--- frontend/src/components/BookingRequestList.jsx
+++ frontend/src/components/BookingRequestList.jsx
@@ -3,81 +3,132 @@ import axios from "axios";
 import { useUserContext } from "../context/UserContext";
 import useBookingRequests from "../hooks/useBookingRequests";
 
-function BookingRequestList () {
+function BookingRequestList() {
+  const { currentUser } = useUserContext();
+  const { bookingRequests, message, fetchData } =
+    useBookingRequests(currentUser);
 
-    const { currentUser } = useUserContext();
-    const { bookingRequests, message, fetchData } = useBookingRequests(currentUser);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  const handleConfirm = async (bookingId) => {
+    try {
+      await axios.put(`/api/bookings/${bookingId}`, {
+        status: "Confirmed",
+      });
+      fetchData();
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
-    const handleConfirm = async (bookingId) => {
-        try {
-            await axios.put(`/api/bookings/${bookingId}`, {
-                status: 'Confirmed',
-            });
-            fetchData();
-        } catch (error) {
-            console.error(err.message);
-        }
-    };
+  const handleUpdateSeats = async (
+    originalAvailableSeats,
+    requestedSeat,
+    tripId
+  ) => {
+    try {
+      const updatedAvailableSeats = originalAvailableSeats - requestedSeat;
 
-    const handleDecline = async (bookingId) => {
-        try {
-            await axios.put(`/api/bookings/${bookingId}`, {
-                status: 'Declined',
-            });
-            fetchData();
-        } catch (error) {
-            console.error(err.message);
-        }
-    };
+      await axios.put(`/api/trips/${tripId}`, {
+        availableSeats: updatedAvailableSeats,
+      });
+    } catch (error) {
+      console.error("Error updating seats:", error.message);
+    }
+  };
 
-    return (
-        <div className="BookingRequestList in-body">
-            {bookingRequests.length === 0 ? (
-                <p>You have no booking requests!</p>
-                ) : (
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Booking #</th>
-                                <th>Trip #</th>
-                                <th>Departure</th>
-                                <th>Destination</th>
-                                <th>Service Day</th>
-                                <th>Time</th>
-                                <th>Requested Seats</th>
-                                <th>Unit Price</th>
-                                <th>Status</th>
-                                <th className="action-column">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {bookingRequests.map((bookingRequest) => (
-                                <tr key={bookingRequest.id}>
-                                    <td>{bookingRequest.id}</td>
-                                    <td>{bookingRequest.tripId}</td>
-                                    <td>{bookingRequest.trip.departure}</td>
-                                    <td>{bookingRequest.trip.destination}</td>
-                                    <td>{bookingRequest.trip.serviceDay}</td>
-                                    <td>{bookingRequest.trip.departureTime}</td>
-                                    <td>{bookingRequest.requestedSeat}</td>
-                                    <td>{bookingRequest.trip.unitPrice}</td>
-                                    <td>{bookingRequest.status}</td>
-                                    <td className="action-column">
-                                        <button className="green-button" onClick={() => handleConfirm(bookingRequest.id)}>Confirm</button>
-                                        <button className="yellow-button" onClick={() => handleDecline(bookingRequest.id)}>Decline</button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                )
-            }
+  const handleDecline = async (bookingId) => {
+    try {
+      await axios.put(`/api/bookings/${bookingId}`, {
+        status: "Declined",
+      });
+      fetchData();
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  return (
+    <div className="BookingRequestList in-body">
+      {bookingRequests.length === 0 ? (
+        <div className="no-booking-request">
+          <div className="body-item">
+            <p>You have no booking requests being sent through!</p>
+            <p>
+              Want to review your offered trips?{" "}
+              <button className="green-button" href="/offeredtrips">
+                CLICK HERE
+              </button>
+            </p>
+          </div>
+          <img
+            className="body-item"
+            src="../src/assets/Nobookingrequest.jpg"
+            style={{ width: "400px", height: "auto" }}
+          />
         </div>
-    )
+      ) : (
+        <table className="fixed-size-table booking-request-table">
+          <thead>
+            <tr>
+              <th>Booking #</th>
+              <th className="trip">Trip #</th>
+              <th>Departure</th>
+              <th>Destination</th>
+              <th className="service-day">Service Day</th>
+              <th className="departure-time">Time</th>
+              <th className="seats">Requested Seats</th>
+              <th className="unit-price">Unit Price</th>
+              <th>Status</th>
+              <th className="action-column">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookingRequests.map((bookingRequest) => (
+              <tr key={bookingRequest.id}>
+                <td>{bookingRequest.id}</td>
+                <td className="trip">{bookingRequest.tripId}</td>
+                <td>{bookingRequest.trip.departure}</td>
+                <td>{bookingRequest.trip.destination}</td>
+                <td className="service-day">
+                  {bookingRequest.trip.serviceDay}
+                </td>
+                <td className="departure-time">
+                  {bookingRequest.trip.departureTime.slice(0, 5)}
+                </td>
+                <td className="seats">{bookingRequest.requestedSeat}</td>
+                <td className="unit-price">{bookingRequest.trip.unitPrice}</td>
+                <td>{bookingRequest.status}</td>
+                <td className="action-column">
+                  <button
+                    className="green-button"
+                    onClick={() => {
+                      handleConfirm(bookingRequest.id);
+                      handleUpdateSeats(
+                        bookingRequest.trip.availableSeats,
+                        bookingRequest.requestedSeat,
+                        bookingRequest.tripId
+                      );
+                    }}
+                  >
+                    Confirm
+                  </button>
+                  <button
+                    className="yellow-button"
+                    onClick={() => handleDecline(bookingRequest.id)}
+                  >
+                    Decline
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
 
 export default BookingRequestList;
