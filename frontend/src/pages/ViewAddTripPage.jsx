9fe1cc8ad13a25295365aca8215939414534fbbd
VeasnaHub <veasna.evan@gmail.com>
2024-01-09 14:02:39 +1300

    deleted commented code

--- frontend/src/pages/ViewAddTripPage.jsx
+++ frontend/src/pages/ViewAddTripPage.jsx
@@ -1,24 +1,3 @@
-// import AddTripDialog from "../components/AddTripDialog";
-// import DashBoardTitle from "../components/DashBoardTitle";
-// import Footer from "../components/Footer";
-// import LoggedNavBar from "../components/LoggedNavBar";
-// import OfferedTripList from "../components/OfferedTripList";
-
-// function ViewAddTripPage() {
-
-//     return (
-//         <div className="ViewAddTripPage">
-//             <LoggedNavBar />
-//             <DashBoardTitle title="YOUR OFFERED TRIPS"/>
-//             <AddTripDialog />
-//             <OfferedTripList />
-//             <Footer />
-//         </div>
-//     )
-// }
-
-// export default ViewAddTripPage;
-
 import AddTripDialog from "../components/AddTripDialog";
 import DashBoardTitle from "../components/DashBoardTitle";
 import Footer from "../components/Footer";
